f6de5767268eaf942d5060fb684bbb65d6cfa60a
beatriz <m.beatrizdepaula13@gmail.com>
2026-06-15 17:33:53 -0300

    parte final - bea

--- backend/apps/reception/models.py
+++ backend/apps/reception/models.py
@@ -16,7 +16,6 @@ class Employee(models.Model):
     name             = models.CharField(max_length=200)
     email            = models.EmailField(null=True, blank=True)
     phone            = models.CharField(max_length=20, null=True, blank=True)
-    role             = models.CharField(max_length=100)   # "Veterinarian", "Receptionist" …
     hire_date        = models.DateField(null=True, blank=True)
     termination_date = models.DateField(null=True, blank=True)
     status           = models.CharField(max_length=20, choices=Status, default=Status.ACTIVE)
